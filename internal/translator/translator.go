6946054da20e06ac597d16b0807479fd9c965c4e
quincinia <eanj13@gmail.com>
2022-04-18 16:13:42 -0700

    Basic translator implementation

--- internal/translator/translator.go
+++ internal/translator/translator.go
@@ -1,2 +1,17 @@
 // Package translator provides operations for performing text substitutions (translations).
 package translator
+
+type CharSet struct {
+	Chars []rune
+	Compl bool
+}
+
+type Translator interface {
+	Translate(chars []rune) ([]rune, error)
+}
+
+type Replacer map[rune]rune
+
+type Deletor map[rune]struct{}
+
+type Squeezer map[rune]struct{}
