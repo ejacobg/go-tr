d3ead4c18ef562ae3da11f25930b55035a4b600b
Jacob Gayban <eanj13@gmail.com>
2022-04-17 18:51:15 -0700

    Added basic control functions (incomplete)

--- internal/control/control.go
+++ internal/control/control.go
@@ -1,1 +1,41 @@
+// Package control parses the control strings from the command line arguments.
 package control
+
+func isASCII(c rune) bool {
+	return c < 128
+}
+
+// Takes two endpoints, and returns all the characters in between them (inclusive).
+// Returns nil if endpoints are non-ASCII characters.
+// Returns nil if end precedes start.
+func expand(start, end rune) (expansion []rune) {
+	if !isASCII(start) || !isASCII(end) {
+		return
+	}
+	if end < start {
+		return 
+	}
+	for start <= end {
+		expansion = append(expansion, start)
+		start++
+	}
+	return
+}
+
+// Generates the character array from the source string.
+func Source(string1 string) (source []rune) {
+	runes := []rune(string1)
+	for i := 0; i < len(runes); i++ {
+		switch c := runes[i]; c {
+		case '-':
+			if i == 0 || i == len(runes)-1 {
+				source = append(source, c)
+			} else {
+
+			}
+		default:
+			source = append(source, c)
+		}
+		}
+	}
+}
