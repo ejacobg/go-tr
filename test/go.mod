984b1b6aa1eced3226a62aa48a8312ea69d3864a
Jacob Gayban <eanj13@gmail.com>
2022-04-19 10:22:39 -0700

    Added integration tests

--- test/go.mod
+++ test/go.mod
@@ -1,3 +1,15 @@
 module it
 
 go 1.18
+
+replace control => ../internal/control
+
+replace input => ../internal/input
+
+replace translator => ../internal/translator
+
+require (
+	control v0.0.0-00010101000000-000000000000
+	input v0.0.0-00010101000000-000000000000
+	translator v0.0.0-00010101000000-000000000000
+)
--- test/it_test.go
+++ test/it_test.go
@@ -1,16 +1,59 @@
 // Package it contains integration tests.
 package it
 
-import "testing"
+import (
+	"control"
+	"input"
+	"strings"
+	"testing"
+	"translator"
+)
 
 func TestInputParsing(t *testing.T) {
-	t.Run("input is correctly gathered and parsed", func(t *testing.T) {})
+	t.Run("input is correctly gathered and parsed", func(t *testing.T) {
+		inp := strings.NewReader("[a-d]")
+		var cs translator.CharSet
+		cs = input.GetChars(inp)
+		cs = control.Parse(string(cs))
+		got := string(cs)
+		want := "[abcd]"
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
 
 func TestParsedTranslators(t *testing.T) {
-	t.Run("replacer accepts parsed input", func(t *testing.T) {})
-	t.Run("deleter accepts parsed input", func(t *testing.T) {})
-	t.Run("squeezer accepts parsed input", func(t *testing.T) {})
+	inp1 := strings.NewReader("[a-z] ")
+	inp2 := strings.NewReader("[A-Z] ")
+	var string1, string2 translator.CharSet
+	string1, string2 = input.GetChars(inp1), input.GetChars(inp2)
+	string1, string2 = control.Parse(string(string1)), control.Parse(string(string2))
+
+	t.Run("replacer accepts parsed input", func(t *testing.T) {
+		inp := []rune("the quick brown fox jumps over the lazy dog")
+		tr := translator.NewReplacer(string1, string2, nil)
+		got := string(tr.Translate(inp))
+		want := "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("deleter accepts parsed input", func(t *testing.T) {
+		inp := []rune("the quick brown fox jumps over the lazy dog")
+		d := translator.NewDeleter(string1, nil)
+		got := string(d.Translate(inp))
+		want := ""
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("squeezer accepts parsed input", func(t *testing.T) {
+
+	})
+
 }
 
 func TestErrorHandling(t *testing.T) {
