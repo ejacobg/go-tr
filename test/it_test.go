7bfc6f6d8e9631a6c0956ca1bc1beb1e1ec7c51d
Jacob Gayban <eanj13@gmail.com>
2022-04-19 08:39:29 -0700

    Added integration test stubs

--- test/it_test.go
+++ test/it_test.go
@@ -2,3 +2,17 @@
 package it
 
 import "testing"
+
+func TestInputParsing(t *testing.T) {
+	t.Run("input is correctly gathered and parsed", func(t *testing.T) {})
+}
+
+func TestParsedTranslators(t *testing.T) {
+	t.Run("replacer accepts parsed input", func(t *testing.T) {})
+	t.Run("deleter accepts parsed input", func(t *testing.T) {})
+	t.Run("squeezer accepts parsed input", func(t *testing.T) {})
+}
+
+func TestErrorHandling(t *testing.T) {
+	t.Run("empty charsets are handled appropriately", func(t *testing.T) {})
+}
